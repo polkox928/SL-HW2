7c5b9d427461df71911cbc433c78ce47cc4f4f5a
polkox928 <polkox928@gmail.com>
2017-06-19 13:26:26 +0200

    Read Labels

--- Code.R
+++ Code.R
@@ -1,1 +1,12 @@
-x = 1
+
+# Read the data -----------------------------------------------------------
+
+# Read the song's labels
+labels = read.table("data/Labels.txt")
+colnames(labels) = c('Labels')
+
+# Package to read the audio files
+install.packages("wrassp")
+library(wrassp)
+
+
